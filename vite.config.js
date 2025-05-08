7bc468269601fff4f6c8b663b191346f8dfa9c94
SandroMaglione <lass.maglio@gmail.com>
2025-05-08 16:04:37 +0200

    init project

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
+});
